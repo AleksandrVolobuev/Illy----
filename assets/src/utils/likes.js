e790bf8d520279417c68ad1edd0f2a6f90bcb159
Illia <135041280+IlliaVolobuev@users.noreply.github.com>
2024-12-06 22:20:15 +0100

    fix likes.js

--- assets/src/utils/likes.js
+++ assets/src/utils/likes.js
@@ -8,11 +8,15 @@ function likeHandler(e) {
     // Проверяем, есть ли "9" в тексте и заменяем ее на "10"
     if (likesP.textContent.includes("9")) {
         likesP.textContent = likesP.textContent.replace("9", "10");
+        likesImg.setAttribute("src","./assets/img/svg/icon-heart-dark-added.svg")
     } else if(likesP.textContent.includes("10")){
         likesP.textContent = likesP.textContent.replace("10", "9");
+        likesImg.setAttribute("src","./assets/img/svg/icon-heart-dark.svg")
     } else {
         console.warn("Число 9 не найдено в тексте.");
     }
+
+    
 }
 
 // Добавляем EventListener корректно
