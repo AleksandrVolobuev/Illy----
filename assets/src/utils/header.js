9d5dcb03d675ba0124bb66c83911fba67f61cd1b
Alek Volobuev <aleksandrvolobuev7676@gmail.com>
2024-12-07 20:00:22 +0200

    fix 1.1.1 style.css

--- assets/src/utils/header.js
+++ assets/src/utils/header.js
@@ -1,9 +1,9 @@
 // Оголосити змінні для ключових елементів сторінки
-const iconMenu = document.querySelector(".img"); // Іконка меню
+const iconMenu = document.querySelector(".btn-navigechen>img"); // Іконка меню
 const list = document.querySelector(".list"); // Список меню
 const nav = document.querySelector(".header_nav"); // Контейнер nav
 const header = document.querySelector(".heder"); // Елемент header
-const btn = document.querySelector(".bthhh"); // Кнопка
+const btn = document.querySelector(".btn-navigechen"); // Кнопка
 
 // Встановити початковий стиль для header
 // header.style.display = "flex";
@@ -21,7 +21,7 @@ function addMenu() {
 
     iconMenu.src = "./assets/img/svg/menu-open-light.svg"; // Змінити іконку на меню
 
-    nav.style.height = "auto"; // Встановити висоту nav
+    //nav.style.height = "auto"; // Встановити висоту nav
 
     header.style.alignItems = "center"; // Вирівняти вміст header
 
@@ -31,9 +31,9 @@ function addMenu() {
 
     list.classList.add("active"); // Додати клас active
 
-    iconMenu.src = "./assets/img/svg/menu-open-dark.svg"; // Змінити іконку на закриття
+    iconMenu.src = "./assets/img/jpg/photo_2024-12-07_11-29-00.jpg"; // Змінити іконку на закриття
 
-    nav.style.height = "100vh"; // Зробити меню на весь екран
+    //nav.style.height = "100vh"; // Зробити меню на весь екран
 
     header.style.alignItems = "flex-start"; // Вирівняти вміст header зверху
 
